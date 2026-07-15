170359d96efb7a56c89d08a56ebf57f23d2ae0be
sugarban-p <271395910+sugarban-p@users.noreply.github.com>
2026-07-16 03:54:54 +0800

    add: 商品列表api - products - items

--- routes/products.js
+++ routes/products.js
@@ -5,35 +5,82 @@ import pool from "../utils/connect-mysql.js";
 
 const router = Router();
 
-const getProductListData = async () => {
-  const sql = "SELECT * FROM products ORDER BY id ASC;";
+const getPetTypeData = async () => {
+  const sql = "SELECT * FROM product_pet_tags;";
   const [rows] = await pool.query(sql);
-  return {
-    success: true,
-    rows,
-  };
+  return rows;
 };
 
-const getItemListData = async () => {
-  const sql = "SELECT * FROM items ORDER BY id ASC;";
-  const [rows] = await pool.query(sql);
+const getProductListData = async (req) => {
+  let category = req.query.category;
+  let tags = req.query.tags;
+  let sort = req.query.sort;
+  let page = +req.query.page;
+  const perPage = 16;
+
+  if (category === undefined) {
+    category = "all";
+  }
+  if (tags === undefined) {
+    tags = [];
+  }
+  if (sort === undefined) {
+    sort = "sold";
+  }
+  if (isNaN(page)) {
+    page = 1;
+  }
+
+  const t_sql = "SELECT COUNT(*) totalRows FROM products;";
+  const [[{ totalRows }]] = await pool.query(t_sql);
+  let totalPages = 0;
+  let rows = [];
+  if (totalRows > 0) {
+    totalPages = Math.ceil(totalRows / perPage);
+
+    const sql = ` SELECT * FROM products ORDER BY id ASC LIMIT ?,?;`;
+    [rows] = await pool.query(sql, [(page - 1) * perPage, perPage]);
+
+    const productIds = rows.map((p) => p.id);
+    let itemRows = [];
+
+    const itemSql = `SELECT * FROM items WHERE prod_id_fk IN (?) ORDER BY prod_id_fk, id;`;
+
+    [itemRows] = await pool.query(itemSql, [productIds]);
+    const itemMap = {};
+
+    for (const item of itemRows) {
+      if (!itemMap[item.prod_id_fk]) {
+        itemMap[item.prod_id_fk] = [];
+      }
+
+      itemMap[item.prod_id_fk].push(item);
+    }
+    rows = rows.map((product) => ({
+      ...product,
+      items: itemMap[product.id] || [],
+    }));
+  }
+
   return {
     success: true,
-    rows,
+    category,
+    tags,
+    sort,
+    page,
+    perPage,
+    totalRows,
+    totalPages,
+    rows: rows,
   };
 };
 
 router.get("/", (req, res) => {
-  res.json({ route: "products", success: true });
+  return res.redirect("./cat");
 });
 
-router.get("/products", async (req, res) => {
-  const data = await getProductListData();
-  res.json(data);
-});
-
-router.get("/items", async (req, res) => {
-  const data = await getItemListData();
+router.get("/:petType", async (req, res) => {
+  const data = await getProductListData(req);
   res.json(data);
 });
 
