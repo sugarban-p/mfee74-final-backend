dab479524a052d06443ab344deb25670683a9f30
sugarban-p <271395910+sugarban-p@users.noreply.github.com>
2026-07-16 14:46:44 +0800

    add: 新增註解

--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,7 @@ import pool from "../utils/connect-mysql.js";
 
 const router = Router();
 
+// 排序選項 對照 sql
 const sortList = {
   default: "total_sold ASC",
   latest: "p.created_at ASC",
@@ -12,6 +13,7 @@ const sortList = {
   price_desc: "p.price DESC",
 };
 
+// 寵物類別列表
 const getPetTypeData = async () => {
   const sql = "SELECT * FROM product_pet_tags;";
   const rows = await pool.query(sql);
@@ -19,6 +21,7 @@ const getPetTypeData = async () => {
 };
 const petTypeList = await getPetTypeData();
 
+// 商品類別列表
 const getCategoryData = async () => {
   const sql = "SELECT * FROM product_category_tags;";
   const rows = await pool.query(sql);
@@ -26,6 +29,7 @@ const getCategoryData = async () => {
 };
 const categoryList = await getCategoryData();
 
+// 依商品(篩選後)讀取底下的所有品項
 const attachItemsToProducts = async (products) => {
   const productIds = products.map((p) => p.id);
   const sql = `SELECT * FROM items WHERE prod_id_fk IN (?) ORDER BY prod_id_fk, id;`;
@@ -43,6 +47,7 @@ const attachItemsToProducts = async (products) => {
   }));
 };
 
+// 讀取商品列表
 const getProductListData = async (req) => {
   const petType = req.params.petType;
   const category = req.query.category || "";
@@ -54,11 +59,11 @@ const getProductListData = async (req) => {
   let page = +req.query.page || 1;
   const perPage = 16;
 
+  // 篩選條件
   const petId = petTypeList.find((elem) => elem["tag_slug"] === petType).id;
   const categoryId = category.length
     ? categoryList.find((elem) => elem["tag_slug"] === category).id
     : 0;
-
   const filters = [`WHERE p.pet_tag_id_fk=${petId}`];
   if (categoryId) {
     filters.push(`category_id_fk=${categoryId}`);
@@ -70,33 +75,34 @@ const getProductListData = async (req) => {
     filters.push(`price<=${priceMax}`);
   }
   const sql_filter = filters.join(" AND ");
+
+  // 讀取商品數量(pagination用)
   const t_sql = `SELECT COUNT(*) totalRows FROM products p ${sql_filter} ;`;
   const [[{ totalRows }]] = await pool.query(t_sql);
+
   let totalPages = 0;
   let rows = [];
   if (totalRows > 0) {
     totalPages = Math.ceil(totalRows / perPage);
-
     const sql_sort = `ORDER BY ${sortList[sort]} , p.id ASC`;
-
     const sql = `
-  SELECT
-    p.*,
-    COALESCE(item_stats.total_sold, 0) AS total_sold,
-    COALESCE(item_stats.total_stock, 0) AS total_stock
-  FROM products p
-  LEFT JOIN (
-    SELECT
-      prod_id_fk,
-      SUM(sold) AS total_sold,
-      SUM(stock) AS total_stock
-    FROM items
-    GROUP BY prod_id_fk
-  ) item_stats ON item_stats.prod_id_fk = p.id
-  ${sql_filter}
-  ${sql_sort}
-  LIMIT ?,?;
-`;
+      SELECT
+        p.*,
+        COALESCE(item_stats.total_sold, 0) AS total_sold,
+        COALESCE(item_stats.total_stock, 0) AS total_stock
+      FROM products p
+      LEFT JOIN (
+        SELECT
+          prod_id_fk,
+          SUM(sold) AS total_sold,
+          SUM(stock) AS total_stock
+        FROM items
+        GROUP BY prod_id_fk
+      ) item_stats ON item_stats.prod_id_fk = p.id
+      ${sql_filter}
+      ${sql_sort}
+      LIMIT ?,?;
+      `;
     [rows] = await pool.query(sql, [(page - 1) * perPage, perPage]);
 
     if (rows.length) rows = await attachItemsToProducts(rows);
