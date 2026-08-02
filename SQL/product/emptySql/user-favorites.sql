147d416ca480b8e4d8f0b7346d75128286164bfb
sugarban-p <271395910+sugarban-p@users.noreply.github.com>
2026-08-02 20:04:05 +0800

    docs: 更新商品mock-data+調整順序

--- SQL/product/emptySql/user-favorites.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO
-    `user_favorites` (
-        `id`,
-        `user_id_fk`,
-        `prod_id_fk`,
-        `created_at`
-    )
-VALUES ();
---
+++ SQL/product/user-favorites.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    `user_favorites` (`user_id_fk`, `prod_id_fk`)
+VALUES (3, 2),
+    (3, 19),
+    (3, 21),
+    (3, 31),
+    (3, 41),
+    (3, 53);
